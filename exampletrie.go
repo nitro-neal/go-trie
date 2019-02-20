50243051fbbb3d1a99c6cceb5ae4135b53cbfdef
Neal Roessler <groessler24@gmail.com>
2019-02-20 11:23:23 +0800

    Added TrieNode struct

--- exampletrie.go
+++ exampletrie.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+type TrieNode struct {
+	character string
+	children map[string]TrieNode
+	completeWord bool
+}
+
 func main() {
     fmt.Println("Example Trie Implementation")
 }
