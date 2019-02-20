8d346587efdb55ac268270cc17213014a54e2b04
Neal Roessler <groessler24@gmail.com>
2019-02-20 12:03:48 +0800

    Added matchprefix and autocomplete implementations

--- exampletrie.go
+++ exampletrie.go
@@ -32,6 +32,36 @@ func (node TrieNode) addPhrase(phrase string) {
 	node.children[firstChar].addPhrase(phrase[1:])
 }
 
+func matchPrefixRecursive(currentNode TrieNode, phrase string) TrieNode {
+
+	fmt.Println(phrase)
+	
+	if phrase == "" {
+		return currentNode
+	}
+
+	firstChar := string(phrase[0])
+
+	if _, ok := currentNode.children[firstChar]; ok == false {
+		return TrieNode{}
+	} else {
+		return matchPrefixRecursive(currentNode.children[firstChar], phrase[1:])
+	}
+}
+
+func autocomplete(currentNode TrieNode, builtString string, initialSearchString string) string {
+
+	if currentNode.completeWord == true {
+		fmt.Println("Completed Word: " + initialSearchString + builtString)
+	}
+
+	for _, v:= range currentNode.children {
+		autocomplete(v, builtString + v.character, initialSearchString)
+	}
+
+	return ""
+}
+
 func addWordsToTrie(rootNode TrieNode, filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
