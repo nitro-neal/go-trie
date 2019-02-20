1962e0eb2d4276aec8276cef35edd587b2257af6
Neal Roessler <groessler24@gmail.com>
2019-02-20 11:52:35 +0800

    Added os, log, bufio dependency. Created function to read all words from text file and placeholder function to add them to trie.

--- exampletrie.go
+++ exampletrie.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"bufio"
 )
 
 type TrieNode struct {
@@ -10,6 +13,31 @@ type TrieNode struct {
 	completeWord bool
 }
 
+func (node TrieNode) addPhrase(phrase string) {
+	fmt.Println("Will add phrase to trie: ", phrase)
+}
+
+func addWordsToTrie(rootNode TrieNode, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		rootNode.addPhrase(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
     fmt.Println("Example Trie Implementation")
+    var rootNode = TrieNode{"", make(map[string]TrieNode), true}
+
+    // Adds all words in dictionary to trie
+	addWordsToTrie(rootNode, "./words_alpha.txt")
 }
