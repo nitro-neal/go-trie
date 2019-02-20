4ef291b83744ad8cd8b448005a8adf9691c87b3a
Neal Roessler <groessler24@gmail.com>
2019-02-20 12:20:33 +0800

    Created interface for user to search the dictionary

--- exampletrie.go
+++ exampletrie.go
@@ -33,8 +33,6 @@ func (node TrieNode) addPhrase(phrase string) {
 }
 
 func matchPrefixRecursive(currentNode TrieNode, phrase string) TrieNode {
-
-	fmt.Println(phrase)
 	
 	if phrase == "" {
 		return currentNode
@@ -84,10 +82,30 @@ func main() {
     fmt.Println("This will add all words in a text dictionary to a trie implemtation and retrieve them in an auto complete fashion.")
     fmt.Println("Usage: go 'run exampletrie.go' to use default dictionary path or go 'run exampletrie.go path/to/dictfile' to use custom dictionary\n\n")
     
+	args := os.Args
+
     var rootNode = TrieNode{"", make(map[string]TrieNode), true}
 
     // Adds all words in dictionary to trie
-	addWordsToTrie(rootNode, "./words_alpha.txt")
+    if len(args) == 1 {
+		addWordsToTrie(rootNode, "./words_alpha.txt")
+	} else {
+		addWordsToTrie(rootNode, args[1])
+	}
 
 	fmt.Println("All words in dictionary added to trie")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text := ""
+	searchString := ""
+
+	for searchString != "exit" {
+		fmt.Print("\n\nEnter text to search for: ")
+		text, _ = reader.ReadString('\n')
+
+		searchString = string(text[:len(text) - 1])
+		currentNode := matchPrefixRecursive(rootNode, searchString);
+		autocomplete(currentNode, "", searchString)
+	}
 }
